56e121a6a093a108a3b0012d8e92bf41a37ae272
MaiiMostafa98 <maiimostafa2841998@gmail.com>
2025-10-02 08:01:51 +0300

    update the project page

--- app/Projects/page.jsx
+++ app/Projects/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import myImage from '../images/bakery.jpg'
+
 
 export default function page() {
   return <>
@@ -10,13 +12,69 @@ export default function page() {
             <h1 className='h2'>My Projects</h1>
         </div>
 
-        
+
+     <div  >
+         <h4 className='mt-5 mb-3 ps-2 textShadow'> -Technologies Used : <strong> </strong>HTML5, CSS3</h4>
+            
+            <div className='row mb-5 '>
+
+             <div className=' col-sm-12 col-md-4 projects mb-3'>
+              <div className='    '>
+                  <h2>Bakery WebSite</h2>
+                  <p><strong>Description : </strong>Designed and developed a responsive website for a bakery, showcasing its products, about section, and contact information in a clean and attractive layout.</p>
+                  <p><strong>Details : </strong>
+                   Structured the content into clear sections (Home, Products, About Us, Contact).</p>
+                   <p>Styled the entire website using modern CSS techniques.
+                   Applied a color palette and imagery that reflects the bakery’s brand identity.
+                   Ensured the website is fully responsive design to ensure smooth experience on all devices.</p>
+                   <a href="https://maiimostafa98.github.io/Bakery/" target="_blank"> <button >Project Link</button></a>
+              </div>
+            </div>
+
+
+            <div className=' col-sm-12 col-md-4 projects-second mb-3  '>
+              <div className='  justify-content-end  '>
+                  <h2>Fokir Website</h2>
+                  <p><strong>Description : </strong>Designed and developed a clean and responsive freelancer website to showcase  skills, services, and past work in a user-friendly layout.</p>
+                  <p><strong>Details : </strong>
+                  Structured the website into main sections (Home, About, Services, Portfolio, Blog, Contact).</p>Used modern CSS for styling and layout consistency.  
+                  Focused on clear typography and minimal design to highlight content.  
+                  Implemented responsive design to ensure smooth experience on all devices.
+                  <p></p>
+                   <a href="https://maiimostafa98.github.io/fokiir/" target="_blank"> <button >Project Link</button></a>
+              </div>
+            </div>
+
+
+             <div className=' col-sm-12 col-md-4 projects mb-3 '>
+              <div className=' justify-content-end '>
+                  <h2>Mealify Website</h2>
+                  <p><strong>Description:</strong> Designed and developed a clean, modern, and responsive restaurant website to showcase professional chefs and promote healthy food options.</p>
+                  <p><strong>Details : </strong> Structured the website into main sections (Home, Chefs, Gallery, Contact).</p>
+                  <p>Applied modern CSS techniques for consistent layout and visual appeal.
+                  Focused on clean typography and minimalistic design to highlight content.
+                  Ensured full responsiveness for smooth user experience across all devices.</p>
+                   <a href="https://maiimostafa98.github.io/Mealify/" target="_blank"> <button >Project Link</button></a>
+              </div>
+            </div>
+
+          
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+{/*         
         <div >
             <div className='  projects mb-3'>
               <div className='  justify-content-end project-card '>
-                  {/* <div className='d-flex jusify-content-end'>
-                    <Image src={myImage} alt='bakery' className='img '></Image>
-                  </div> */}
                   <h2>Bakery WebSite</h2>
                   <p><strong>Description : </strong>Designed and developed a responsive website for a bakery, showcasing its products, about section, and contact information in a clean and attractive layout.</p>
                   <p><strong>Technologies Used : </strong>HTML5, CSS3</p>
@@ -33,9 +91,6 @@ export default function page() {
 
             <div className='  projects mb-3 '>
               <div className='  justify-content-end project-card '>
-                  {/* <div className='d-flex jusify-content-end'>
-                    <Image src={myImage} alt='bakery' className='img '></Image>
-                  </div> */}
                   <h2>Fokir Website</h2>
                   <p><strong>Description : </strong>Designed and developed a clean and responsive freelancer website to showcase  skills, services, and past work in a user-friendly layout.</p>
                   <p><strong>Technologies Used : </strong>HTML5, CSS3</p>
@@ -52,9 +107,6 @@ export default function page() {
 
             <div className='  projects mb-3 '>
               <div className='  justify-content-end project-card '>
-                  {/* <div className='d-flex jusify-content-end'>
-                    <Image src={myImage} alt='bakery' className='img '></Image>
-                  </div> */}
                   <h2>Mealify Website</h2>
                   <p><strong>Description:</strong> Designed and developed a clean, modern, and responsive restaurant website to showcase professional chefs and promote healthy food options.</p>
                   <p><strong>Technologies Used : </strong>HTML5, CSS3</p>
@@ -69,7 +121,10 @@ export default function page() {
             <div className='line1'></div>
             
            
-        </div>
+        </div> */}
+
+
+
 
    </div> 
    </div>
