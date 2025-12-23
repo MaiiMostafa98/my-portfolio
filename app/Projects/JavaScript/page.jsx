d362d8832ebb28366d87dae366cd8578d9337eb5
MaiiMostafa98 <maiimostafa2841998@gmail.com>
2025-12-24 01:28:07 +0200

    Added new project

--- app/Projects/JavaScript/page.jsx
+++ app/Projects/JavaScript/page.jsx
@@ -22,8 +22,22 @@ export default function page() {
              </div>
              </div>
 
+            </div>
 
-           
+             <div className='row  pb-3'>
+
+             <div className=' col-sm-12 col-md-12 projects1  '>
+             <div className="text-center">
+             <h2 className="text-center">Book Marker</h2>
+             <p><strong>Description:</strong> A simple and elegant web site (CRUD System) that allows users to save, manage, and quickly access their favorite websites . </p>
+             <p><strong>Details:</strong>This project features a clean, minimalistic design with a strong focus on usability and user experience. Users can add, update, delete, and visit bookmarks easily. The app uses LocalStorage to persist data, ensuring that bookmarks are saved even after refreshing the page or closing the browser.</p>
+             <p>Built using HTML, CSS, Bootstrap, and JavaScript. The layout is fully responsive and optimized for seamless performance across all devices, including desktops, tablets, and mobile screens..</p>
+             <div className="d-flex justify-content-center">
+             <a href="https://maiimostafa98.github.io/BookMarker/" target="_blank" rel="noopener noreferrer">
+             <button>Project Link</button></a>
+             </div>
+             </div>
+             </div>
 
             </div>
             </div>
