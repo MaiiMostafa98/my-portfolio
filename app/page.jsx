1506dfe34d9f6c518f21f287e20f3b9804a13191
MaiiMostafa98 <maiimostafa2841998@gmail.com>
2026-01-20 22:56:36 +0200

    edit

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import myImage from '../app/images/3.png'
+// import myImage from '../app/images/3.png'
+import myImage from './images/ai-maii.jpeg'
 import React from 'react'
 
 export default function page() {
@@ -11,7 +12,27 @@ export default function page() {
   
    <div className='home-intro ' >
     <div className="home-card ">
-        <div className='row '>
+        <div className='row pt-5'>
+
+
+          {/* <div className='col-12'>
+               <div className="home-img ">
+                  <Image src={myImage} alt='myPic' />
+               </div>
+          </div>
+
+
+          <div className='col-12 text-center'>
+             <div>
+                <h4>Hello, I'm <strong className='h3'>  Maii Mostafa</strong> </h4>
+    
+                <h1>Front-end Web Developer</h1>
+                <p className="p h5">"I create clean, responsive, and modern user interfaces with a strong focus on usability and user experience. I bring ideas to life through thoughtful, detail-oriented design and a passion for turning complex problems into intuitive digital solutions. </p>
+                <Link href="/Projects"><button ><strong>View My Projects</strong></button></Link>
+                <Link href="/About"><button className='bt-gray' ><strong>More About Me</strong></button></Link>
+             </div>
+          </div> */}
+
 
 
          <div className='col-12 col-md-4 mb-4 '>
@@ -27,7 +48,6 @@ export default function page() {
     
                 <h1>Front-end Web Developer</h1>
                 <p className="p h5">"I create clean, responsive, and modern user interfaces with a strong focus on usability and user experience. I bring ideas to life through thoughtful, detail-oriented design and a passion for turning complex problems into intuitive digital solutions. </p>
-                {/* <button ><strong><Link href="/About">More About Me</Link></strong></button> */}
                 <Link href="/Projects"><button ><strong>View My Projects</strong></button></Link>
                 <Link href="/About"><button className='bt-gray' ><strong>More About Me</strong></button></Link>
              </div>
