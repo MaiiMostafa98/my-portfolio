ab2dc4e178253184440c1311c7cd716ce47ca66e
MaiiMostafa98 <maiimostafa2841998@gmail.com>
2026-04-12 19:12:41 +0200

    describe what you changed

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,89 +1,132 @@
 import Link from 'next/link';
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 
 export default function page() {
   return <>
-  <div className="pageintro" >
-    <div className="page-card">
-        <div className='aboutme'>
-            <h1 className='h2'>About Me</h1>
-        </div>
   
+  
+    <div className="pageintro" >
+    <div className="page-card">
+        {/* <div className='aboutme'>
+            <h1 className='h2'>About Me</h1>
+        </div> */}
+  <div className='container'>
   <div className='  row '>
 
 
-    <div className='leftSide col-md-4  '>
-      <table>
-      <tbody>
-         <tr>
-          <td className='pb-5'>Address:</td>
-          <td className='ps-3 pb-5'>New Cairo , Egypt</td>
-        </tr>
-        <tr>
-          <td className='pb-5'>Phone :</td>
-          <td className='ps-3 pb-5'>01145446083</td>
-        </tr>
-        <tr>
-          <td className=''>E-mail :</td>
-          <td className='ps-3 '>MaiiMustafa_98@outlook.com</td>
-        </tr>
-
-      </tbody>
-    </table>
-
-      <div className='me-5 my-5'>
-         <Link href="/Projects"><button ><strong>View My Projects</strong></button></Link>
-         <a href="/Maii-Mostafa-Front-End-Developer.pdf" download={"Maii-Mostafa-CV.pdf"} className=""><button ><strong>Download CV</strong></button></a>
-
-         {/* <Link href="https://drive.google.com/file/d/1VsiOTWLPwPqPXL5xV80ZHZHSTWC7PYoy/view?usp=sharing" target="_blank"><button ><strong>Download CV</strong></button></Link> */}
-     </div>
-       
-       {/* <button ><a href="https://www.linkedin.com/in/maii-mostafa-b11a47188/" target="_blank">Linked-In</a></button>     */}
-
-    </div>
-
-
-    <div className='rightSide  col-md-8'>
-        <div className='pt-4 pb-4'>
-        <h4>My Skills: </h4>
+    <div >
+        <div className=' pb-2 '>
+        <h3 className='text-decoration-underline pt-3 '>My Skills: </h3>
         <div>
-          <p className='pt-3'>HTML 95%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" width" ></div>
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" width rounded " >
+             <p className='text-light mx-3'>HTML 95%</p>
+            </div>
            </div>
-           <p className='pt-3'>CSS 85%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" width-85" ></div>
+           
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" width-85 rounded" >
+              <p className='text-light mx-3'>CSS 85%</p>
+            </div>
            </div>
-           <p className='pt-3'>Bootstrap 85%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" width-85" ></div>
+           
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" width-85 rounded" >
+              <p className='text-light mx-3'>Bootstrap 85%</p>
+            </div>
            </div>
-           <p className='pt-3'>javaScript 75%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" w-75 bg-lavander" ></div>
+           
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" w-75 bg-lavander rounded" >
+              <p className='text-light mx-3'>javaScript 75%</p>
+            </div>
            </div>
-           <p className='pt-3'>React.js 50%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" w-50 bg-lavander" ></div>
+          
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" w-50 bg-lavander rounded" >
+             <p className='text-light mx-3'>React.js 50%</p>
+            </div>
            </div>
-           <p className='pt-3'>Next.js 50%</p>
-           <div className="progress " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className=" w-50 bg-lavander" ></div>
+           
+           <div className="progresss rounded mt-4 " role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className=" w-50 bg-lavander rounded" >
+             <p className='text-light mx-3'>Next.js 50%</p>
+            </div>
            </div>
+
+          <div className=''>
+              <Link href="/Projects"><button className='bt-gray' ><strong>View My Projects</strong></button></Link>
+              <a href="/Maii-Mostafa-Front-End-Developer.pdf" download={"Maii-Mostafa-CV.pdf"} className=""><button ><strong>Download CV</strong></button></a>
+          </div>
 
         </div>
       </div>
     </div>
 
-  </div>  
+  </div> 
+
+  <div className='pt-3 pb-3'>
+     <h3 className='text-decoration-underline pt-3 pb-3 '>Contact me </h3>
+
+      <div className="row g-3">
+
+  {/* Phone */}
+  <div className="col-md-3">
+    <div className=" py-3 px-3 rounded text-center  shadow">
+      <div className='icon m-auto'>
+        <FontAwesomeIcon icon={faPhone} size="lg"  />
+      </div>
+      <p className="mb-0 fw-bold mt-2">01145446083</p>
+    </div>
+  </div>
+
+  {/* Email */}
+  <div className="col-md-3">
+    <div className=" py-3 px-3 rounded text-center  shadow">
+      <div className='icon m-auto'>
+        <FontAwesomeIcon icon={faEnvelope} size="lg"  />
+      </div>
+      <p className="mb-0 fw-bold mt-2">MaiiMustafa_98@outlook.com</p>
+    </div>
+  </div>
+
+  {/* LinkedIn */}
+  <div className="col-md-3">
+    <a href="https://www.linkedin.com/in/maii-mostafa-b11a47188/" target="_blank" className="text-decoration-none text-secondary ">
+      <div className="py-3 px-3 rounded text-center  shadow">
+        <div className='icon m-auto'>
+          <FontAwesomeIcon icon={faLinkedin} size="lg"  />
+        </div>
+        <p className="mb-0 fw-bold mt-2">LinkedIn</p>
+      </div>
+    </a>
+  </div>
+
+  {/* GitHub */}
+  <div className="col-md-3">
+    <a href="https://github.com/MaiiMostafa98" target="_blank" className="text-decoration-none text-secondary">
+      <div className="py-3 px-3 rounded text-center  shadow">
+          <div className='icon m-auto'>
+            <FontAwesomeIcon icon={faGithub} size="lg"  />
+            </div>
+           <p className="mb-0 fw-bold mt-2">GitHub</p>
+      </div>
+    </a>
+  </div>
+
+</div>
+     
+  </div> 
 
  
 
    </div> 
    </div>
+  </div>
   </>
 }
